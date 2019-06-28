0af588a2595fe1b64b08ee0ed6ac372a83acae5e
Aditya Mangalampalli <Aditya_Mangalampalli@epam.com>
2019-06-28 16:23:42 +0530

    home-task-1

---
+++ app.js
@@ -0,0 +1,6 @@
+import * as config from "./config/json";
+import { User, Product } from "./models";
+
+console.log(config.name);
+let user = new User();
+let product = new Product();
---
+++ config/json.js
@@ -0,0 +1,1 @@
+export const name="Node.js Homework Application"
